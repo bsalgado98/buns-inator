52701a2ba2e860bab190f20fcce3a57364b60daf
Bruno Salgado <bsalgado@stevens.edu>
2022-05-14 13:04:23 -0400

    added websockets client support

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,39 @@ import BunsButton from './components/BunsButton/BunsButton'
 import { useSelector } from 'react-redux'
 import { COLOR } from './utility/constants'
 import WordInfoInterface from './interfaces/WordInfo.interface';
+import WordCardInterface from './interfaces/WordCard.interface';
+import { useCallback, useEffect, useState } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 function App() {
 
     const { prefixes, suffixes }
         : { prefixes: WordInfoInterface[], suffixes: WordInfoInterface[] }
         = useSelector((state: any) => state.wordList)
+    
+    const ps = useSelector((state: any) => state.wordModifiers.prefixes) as WordCardInterface[]
+    const [socketUrl, setSocketUrl] = useState('ws://localhost:8000')
+    const [messageHistory, setMessageHistory] = useState([])
+    const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl)
+    const callback = useCallback
+
+    useEffect(() => {
+        if (!lastMessage) return
+
+        setMessageHistory(prev => prev.concat(lastMessage))
+    }, [lastMessage, setMessageHistory])
+
+    useEffect(() => {
+        if (!ps) return
+
+        ps.forEach(prefix => {
+            const message = {
+                event: 'lobby:add_player',
+                payload: prefix.text
+            }
+            callback(() => sendMessage(JSON.stringify(message)), [])
+        })
+    }, [ps])
 
     return (
         <div className="App">
